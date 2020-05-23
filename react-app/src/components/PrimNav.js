772f3a9e08e2ed76a6887ce651aebb774acfd979
Wilson <wilsonj806@gmail.com>
2020-05-23 15:03:56 -0400

    Redesign Nav bar to not be based off of future content quantities

--- react-app/src/components/PrimNav.js
+++ react-app/src/components/PrimNav.js
@@ -1,22 +1,12 @@
 import React from "react";
+
 import "./styles/prim-nav.css";
 
 const PrimaryNav = () => (
   <nav id="primary-nav">
     <div className="nav-header">
-      <h1>Site Title Here</h1>
+      <h1 className="nav-title">Site Title Here</h1>
     </div>
-    <ul className="nav-list">
-      <li className="nav-item">
-        <span>About</span>
-      </li>
-      <li className="nav-item">
-        <span>Media</span>
-      </li>
-      <li className="nav-item">
-        <span>Articles</span>
-      </li>
-    </ul>
   </nav>
 );
 
