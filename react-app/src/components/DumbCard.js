25c209c9e08ca05ac7497e699a5178635f32859d
Wilson <wilsonj806@gmail.com>
2020-05-25 15:29:58 -0400

    Refactor Landing to fetch data to render cards
    Make Landing page fetch data from a Wordpress REST API and render DumbCards with that content. Also update DumbCard to render the content fetched by the Landing page.
    
    Note that we render that content by using dangerouslySetInnerHTML.

--- react-app/src/components/DumbCard.js
+++ react-app/src/components/DumbCard.js
@@ -1,36 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./styles/landing-card.css";
 
-const DumbCard = () => (
-  <section className="card--landing">
-    <header className="card-header" />
-    <div className="card-body">
-      <h3>Article Name</h3>
-      <p className="content--meta">
-        <span className="label--meta">Date added:</span>
-        9999999
-      </p>
-      <p className="content--meta">
-        <span className="label--meta">Estimated Read Time:</span>
-        9999999
-      </p>
-      <p>
-        Suspendisse auctor, lorem iaculis ornare sagittis, turpis sem cursus
-        nisl, a mollis velit quam at massa. Proin sit amet felis feugiat,
-        dapibus sem eu, tincidunt ante. Nullam eu nibh vel felis molestie
-        vehicula. Praesent nunc massa, suscipit vel ipsum id, venenatis
-        malesuada quam. Proin faucibus malesuada leo in maximus. Maecenas
-        ullamcorper ornare urna non porttitor. Nulla facilisi. Nullam hendrerit
-        nisl non porttitor euismod. Nunc purus tellus, tempor eget lorem vel,
-        mattis mollis mauris. Nunc nulla libero, vehicula at ullamcorper at,
-        placerat in leo. Sed sollicitudin facilisis nisi, et efficitur turpis
-        accumsan sit amet.
-      </p>
-      <div className="card-expand">
-        <button className="btn--read-more">Read More</button>
+const DumbCard = ({ article }) => {
+  return (
+    // The actual content will need a dangerouslySetInnerHTML
+    <section className="card--landing">
+      <header className="card-header" />
+      <div className="card-body">
+        <h3>{article.title}</h3>
+        <p className="content--meta">
+          <span className="label--meta">Date added: </span>
+          {article.date}
+        </p>
+        <p className="content--meta">
+          <span className="label--meta">Estimated Read Time: </span>
+          9999999
+        </p>
+        <div
+          className="content--body"
+          dangerouslySetInnerHTML={{ __html: article.excerpt.rendered }}
+        ></div>
+        <div className="card-expand">
+          <button className="btn--read-more" type="button">
+            Continue Reading
+          </button>
+        </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default DumbCard;
--- react-app/src/pages/Landing.js
+++ react-app/src/pages/Landing.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import DumbCard from "../components/DumbCard";
 import BaseLayout from "../layouts/BaseLayout";
 
 const Landing = () => {
+  const [landingArticles, setArticles] = useState([]);
+  useEffect(() => {
+    const asyncFetch = async () => {
+      // TODO add query strings to the thing (e.g '?title=doo&color=red')
+      const api = "http://localhost:8000/wp-json/wp/v2/posts?per_page=5";
+      const res = await fetch(api).then((res) => res.json());
+      console.log(res);
+      const articles = res.map(
+        ({ date, excerpt, link, tags, title, author }) => {
+          return {
+            date,
+            excerpt,
+            link,
+            tags,
+            title: title.rendered,
+          };
+        }
+      );
+      setArticles(articles);
+    };
+    asyncFetch();
+  }, []);
+
+  const MappedArticles =
+    landingArticles.length > 0
+      ? landingArticles.map((article) => <DumbCard article={article} />)
+      : null;
   return (
     <BaseLayout>
       <article className="landing">
@@ -21,11 +48,7 @@ const Landing = () => {
               </div>
             </div>
           </section> */}
-          <section className="blogs-list">
-            <DumbCard />
-            <DumbCard />
-            <DumbCard />
-          </section>
+          <section className="blogs-list">{MappedArticles}</section>
         </div>
       </article>
     </BaseLayout>
