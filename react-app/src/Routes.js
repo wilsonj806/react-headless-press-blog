6b03a77c41e93a8e62e54bb9063aa4fb679ab084
Wilson <wilsonj806@gmail.com>
2020-05-25 17:47:08 -0400

    Add a Blog layout with routing via React Router

--- react-app/src/Routes.js
+++ react-app/src/Routes.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Landing from "./pages/Landing";
+import Blog from "./pages/Blog";
 
 const AppRoutes = () => {
   return (
     <Switch>
       <Route path={["/"]} exact={true} component={Landing} />
+      <Route path="/blog/:id" exact={true} component={Blog} />
     </Switch>
   );
 };
---
+++ react-app/src/pages/Blog.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import BaseLayout from "../layouts/BaseLayout";
+
+const BlogLayout = () => {
+  const location = useParams();
+  useEffect(() => {
+    console.log(location);
+  }, [location]);
+
+  return (
+    <BaseLayout>
+      <article>Hello, this is my blog</article>
+    </BaseLayout>
+  );
+};
+
+export default BlogLayout;
