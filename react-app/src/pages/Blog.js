6c4ef946fc78ac91624febc8ee394e87af824578
Wilson <wilsonj806@gmail.com>
2020-05-27 16:45:02 -0400

    Add data fetching for individual posts

--- react-app/src/pages/Blog.js
+++ react-app/src/pages/Blog.js
@@ -1,17 +1,46 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import BaseLayout from "../layouts/BaseLayout";
 
 const BlogLayout = () => {
-  const location = useParams();
+  const param = useParams();
+  const [article, setArticle] = useState(undefined);
   useEffect(() => {
-    console.log(location);
-  }, [location]);
+    const asyncFetch = async () => {
+      try {
+        const endpoint =
+          "http://localhost:8000/wp-json/wp/v2/posts/" + param.id;
 
+        const res = await fetch(endpoint)
+          .then((res) => res.json())
+          .catch((err) => {
+            throw new Error(err);
+          });
+        console.log(res);
+        setArticle(res);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        console.log("done");
+      }
+    };
+    asyncFetch();
+  }, [param]);
+
+  const ToRender = article ? (
+    <>
+      <h1>{article.title.rendered}</h1>
+      <p>{article.date}</p>
+      <section
+        className="blog--content"
+        dangerouslySetInnerHTML={{ __html: article.content.rendered }}
+      />
+    </>
+  ) : null;
   return (
     <BaseLayout>
-      <article className="layout--content">Hello, this is my blog</article>
+      <article className="layout--content">{ToRender}</article>
     </BaseLayout>
   );
 };
